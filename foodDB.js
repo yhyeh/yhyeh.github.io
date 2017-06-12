25778c3629c890dba61c15ba9ea5084c1fcfb34b
yhyeh <ilmd0103@gmail.com>
2017-06-12 21:46:31 +0800

    interaction

---
+++ foodDB.js
@@ -0,0 +1,175 @@
+//////JSON Format//////
+foods =[
+	//somethimg spicy
+	{
+		mName:"八方 酸辣湯餃", 
+		mProperty:[0,2,1,-1,3,-1,[0,3,4]],
+		mWeight:0
+	},
+	{
+		mName:"漢城 辣炒年糕", 
+		mProperty:[0,2,0,-1,3,0,[3,4]],
+		mWeight:0
+	},
+	//somthing cheap
+	{
+		mName:"二餐三樓",
+		mProperty:[0,0,2,-1,-1,1,[0,2,3,4]],
+		mWeight:0
+	},
+	{
+		mName:"八方 鍋貼or水餃<br>10顆以內", 
+		mProperty:[0,1,0,-1,2,1,[3,4]],
+		mWeight:0
+	},
+	{
+		mName:"姊妹",
+		mProperty:[0,1,1,1,-1,1,[4]],
+		mWeight:0
+	},
+	{
+		mName:"二餐一樓 自助餐<br>四菜無主菜",
+		mProperty:[0,1,1,-1,-1,1,[4]],
+		mWeight:0
+	},
+	//somthing cold
+	{
+		mName:"7-11 涼麵",
+		mProperty:[0,1,0,-1,1,-1,[4]],
+		mWeight:0
+	},
+	{
+		mName:"",
+		mProperty:[0,1,0,-1,1,-1,[4]],
+		mWeight:0
+	},
+	//something hot
+	{
+		mName:"漢城 烤肉湯麵",
+		mProperty:[0,2,1,-1,3,-1,[4]],
+		mWeight:0
+	},
+	{
+		mName:"茗松 牛肉麵",
+		mProperty:[0,3,1,-1,3,-1,[4]],
+		mWeight:0
+	},
+	{
+		mName:"A華 滷味",
+		mProperty:[0,2,-1,-1,3,1,[4]],
+		mWeight:0
+	},
+	{
+		mName:"極麵道",
+		mProperty:[0,2,1,1,3,1,[3,4]],
+		mWeight:0
+	},
+	{
+		mName:"食尚屋 拉麵",
+		mProperty:[0,3,1,-1,3,1,[3,4]],
+		mWeight:0
+	},
+	{
+		mName:"7-11 花雕雞麵",
+		mProperty:[0,1,1,1,3,0,[4]],
+		mWeight:0
+	},
+	//somethig a little bit expensive
+	{
+		mName:"多多 義大利麵",
+		mProperty:[0,3,1,-1,-1,-1,[4]],
+		mWeight:0
+	},
+	{
+		mName:"二餐 燒臘",
+		mProperty:[0,3,2,1,-1,1,[3,4]],
+		mWeight:0
+	},
+	{
+		mName:"八方 鍋貼or水餃<br>10顆以上", 
+		mProperty:[0,2,0,-1,2,1,[3,4]],
+		mWeight:0
+	},
+	//somthing luxurious
+	{
+		mName:"Nola(Lala?)",
+		mProperty:[0,4,-1,1,-1,-1,[1,4]],
+		mWeight:0
+	},
+	{
+		mName:"麥當勞",
+		mProperty:[0,4,-1,1,-1,0,[1,3,4]],
+		mWeight:0
+	},
+	//something large
+	{
+		mName:"茗松 麵食<br>加餛飩 加貢丸 加麵 加榨菜肉絲",
+		mProperty:[0,3,2,-1,-1,-1,[4]],
+		mWeight:0
+	},
+	{
+		mName:"茗松 蓋飯系列<br>加飯",
+		mProperty:[0,2,2,1,-1,1,[4]],
+		mWeight:0
+	},
+	{
+	//something healthy
+	{
+		mName:"7-11 生菜沙拉",
+		mProperty:[0,2,0,0,1,2,[0,4]],
+		mWeight:0
+	},
+	{
+		mName:"女二 水果",
+		mProperty:[0,2,-1,0,-1,2,[0,4]],
+		mWeight:0
+	},
+	{
+		mName:"二餐三樓 水煮餐",
+		mProperty:[0,0,2,0,-1,1,[4]],
+		mWeight:0
+	},
+	{
+		mName:"二餐二樓 素食自助餐",
+		mProperty:[0,2,-1,0,-1,2,[4]],
+		mWeight:0
+	},
+	//something like junk
+	{
+		mName:"一餐 炸物",
+		mProperty:[0,2,-1,1,-1,0,[3,4]],
+		mWeight:0
+	},
+	{
+		mName:"女二 炸物",
+		mProperty:[0,2,-1,1,-1,0,[3,4]],
+		mWeight:0
+	},
+	//something sweet
+	{
+		mName:"小木屋",
+		mProperty:[0,2,-1,1,-1,-1,[1,3,4]],
+		mWeight:0
+	},
+	{
+		mName:"二餐二樓 甜品店",
+		mProperty:[0,2,-1,1,1,-1,[1]],
+		mWeight:0
+	},
+	{
+		mName:"米克Q",
+		mProperty:[0,2,-1,1,0,0,[1]],
+		mWeight:0
+	},
+	{
+		mName:"ComeBuy",
+		mProperty:[0,2,-1,1,0,0,[1]],
+		mWeight:0
+	},
+	/*,
+	{
+		mName:"",
+		mProperty:[0,-1,-1,-1,[]],
+		mWeight:0
+	},*/
+];
---
+++ formControl.js
@@ -0,0 +1,92 @@
+//////Option//////
+obstacles = new Array();
+obstacles[0] = ["請先設定障礙"]
+obstacles[1] = ["超缺錢","想省錢","正常吃","微奢侈","超奢侈"];
+obstacles[2] = ["不太餓","餓了","超餓"];
+obstacles[3] = ["減肥中QQ","增肥中ㄏㄏ"];
+obstacles[4] = ["超熱","熱","舒適","冷"];
+obstacles[5] = ["垃圾級","均衡一下","養生級"];
+obstacles[6] = ["酸","甜","苦","辣","鹹"];
+////////////////
+function renew(formId,index){ //renew limit option according to obstacles
+	var o;
+	if(formId == "order1"){
+	  o = document.getElementById("limit1");
+	}
+	else if(formId == "order2"){
+	  o = document.getElementById("limit2");
+	}
+	else if(formId == "order3"){
+	  o = document.getElementById("limit3");
+	}
+
+	for(var i = 0;i < obstacles[index].length;i++)
+		o.options[i]=new Option(obstacles[index][i], obstacles[index][i]);
+	o.length=obstacles[index].length;
+}
+
+function btnEnable(){ // foolproof feature for submit button
+	document.getElementById("notsatisfied").disabled = false;
+	document.getElementById("recommand").disabled = true;
+	document.getElementById("notsatisfied").className = "clickable";
+	document.getElementById("recommand").className = "disclickable";
+}
+
+function checkObs(){ //At least choose one obstacle, or the button will be disabled
+	if(document.getElementById("condition1").selectedIndex != 0){
+		document.getElementById("recommand").disabled = false;
+		document.getElementById("recommand").className = "clickable";
+	}
+	else{
+		document.getElementById("recommand").disabled = true;
+		document.getElementById("recommand").className = "disclickable";
+		document.getElementById("notsatisfied").disabled = true;
+		document.getElementById("notsatisfied").className = "disclickable";
+		document.getElementById("result").style.visibility = "hidden";
+	}
+}
+var repeatTimes = -1; //count the times that user has asked to change the result
+function showResult(){ //determine which food will be recommanded by system and print it out
+	repeatTimes++;
+	if(repeatTimes == 0){
+		for(var i = 0;i < foods.length;i++){
+			foods[i].mWeight = 0;
+		}
+		var cdt = new Array();
+		cdt[0] = document.getElementById("condition1").selectedIndex;
+		cdt[1] = document.getElementById("condition2").selectedIndex;
+		cdt[2] = document.getElementById("condition3").selectedIndex;
+		var lmt = new Array();
+		lmt[0] = document.getElementById("limit1").selectedIndex;
+		lmt[1] = document.getElementById("limit2").selectedIndex;
+		lmt[2] = document.getElementById("limit3").selectedIndex;
+		for(var i = 0;i < foods.length;i++){ //count the weight according to the condition provided by user
+			for(var j = 0;j < cdt.length;j++){
+				if(cdt[j] == 0)
+					continue;
+				if(cdt[j] != 6){
+					if(foods[i].mProperty[cdt[j]] == lmt[j]){
+						foods[i].mWeight += (cdt.length-j);
+					}
+					else if(foods[i].mProperty[cdt[j]] != -1){
+						foods[i].mWeight -= 1;
+					}
+				}
+				else{
+					for(var k = 0;k < foods[i].mProperty[6].length;k++)
+						if(foods[i].mProperty[6][k] == lmt[j])
+							foods[i].mWeight += (cdt.length-j);
+				}
+			}
+		}
+		foods.sort(function(a,b){return b.mWeight - a.mWeight});
+	}
+	if(repeatTimes >= foods.length){
+		window.alert("沒東西可以吃啦 QQ");
+		location.reload();
+		return;
+	}
+	document.getElementById("result").innerHTML = "障礙排除 ※ " + foods[repeatTimes].mName;
+}
+
+console.log(JSON.stringify(foods));
